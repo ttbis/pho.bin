2da2469b211180d69cb4b08f79c2bcf7dae63230
Bislan <bislan_taymaskhanov@mail.ru>
2020-10-01 01:34:49 +0300

    Updates

---
+++ precache-manifest.6b245447770f7887ddf71defb97443b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77695374ea0de0139b4ebc573547780a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b066db31a4e60a02bce2",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "94ba1d73297feccbabc3",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b066db31a4e60a02bce2",
+    "url": "./static/js/2.35a8f7a7.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.35a8f7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ba1d73297feccbabc3",
+    "url": "./static/js/main.3a1aec24.chunk.js"
+  },
+  {
+    "revision": "5f526d65db2de3b6ee8f",
+    "url": "./static/js/runtime-main.0fd8efd7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
